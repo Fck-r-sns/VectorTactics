a55074a9265d8dc7fd7b3b2647c3bd33fd58d218
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-25 14:34:27 +0300

    Camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float smoothing = 5f;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.y = transform.position.y;
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothing * Time.fixedDeltaTime);
+    }
+
+}
