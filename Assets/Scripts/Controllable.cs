529041b94d9635c1e06c8b13cb79efaa6d4e0192
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-27 20:20:49 +0300

    Controllable and ControllableRigidbody

---
+++ Assets/Scripts/Controllable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface Controllable  {
+    void Move(Vector3 direction);
+    void Turn(Vector3 direction);
+    void Shoot();
+}
---
+++ Assets/Scripts/ControllableRigidbody.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ControllableRigidbody : MonoBehaviour, Controllable
+{
+
+    [SerializeField]
+    private float speed = 6.0f;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 direction)
+    {
+        direction.y = 0.0f;
+        rigidbody.MovePosition(transform.position + direction.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    public void Turn(Vector3 direction)
+    {
+        direction.y = 0.0f;
+        rigidbody.MoveRotation(Quaternion.LookRotation(direction));
+    }
+
+    public void Shoot()
+    {
+        throw new NotImplementedException();
+    }
+
+}
