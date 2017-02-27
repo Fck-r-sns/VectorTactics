a4219a3263c1501890d1ac251ddc8ef34298386f
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-27 21:24:26 +0300

    Animator added to SoldierController

--- Assets/Scripts/SoldierController.cs
+++ Assets/Scripts/SoldierController.cs
@@ -4,23 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class ControllableCharacter : MonoBehaviour, Controllable
+[RequireComponent(typeof(SoldierAnimation))]
+public class SoldierController : MonoBehaviour, Controllable
 {
 
     [SerializeField]
     private float speed = 6.0f;
 
     private Rigidbody rigidbody;
+    private SoldierAnimation animator;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<SoldierAnimation>();
     }
 
     public void Move(Vector3 direction)
     {
         direction.y = 0.0f;
         rigidbody.MovePosition(transform.position + direction.normalized * speed * Time.fixedDeltaTime);
+        animator.AnimateMoving(direction);
     }
 
     public void Turn(Vector3 direction)
@@ -31,7 +35,8 @@ public class ControllableCharacter : MonoBehaviour, Controllable
 
     public void Shoot()
     {
-        throw new NotImplementedException();
+        // TODO
+        animator.AnimateShooting();
     }
 
 }
