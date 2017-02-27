7d9d34f64c37238aed865a633fd91f9580039d9e
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-27 21:24:12 +0300

    SoldierAnimation

---
+++ Assets/Scripts/SoldierAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SoldierAnimation : MonoBehaviour
+{
+
+    private Animator animator;
+
+	void Awake () {
+        animator = GetComponent<Animator>();
+	}
+
+    public void AnimateMoving(Vector3 direction)
+    {
+        float angle_rad = transform.eulerAngles.y * Mathf.Deg2Rad;
+        float cos = Mathf.Cos(angle_rad);
+        float sin = Mathf.Sin(angle_rad);
+
+        // transform movement direction to object coordinate system
+        float frontMovement = direction.x * sin + direction.z * cos;
+        float sideMovement = direction.x * cos - direction.z * sin;
+
+        animator.SetFloat("FrontMovement", frontMovement);
+        animator.SetFloat("SideMovement", sideMovement);
+    }
+
+    public void AnimateShooting()
+    {
+        // TODO
+    }
+}
