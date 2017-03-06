5d4f0681471e9eb07e87af14022be7f50a5fe3d5
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-06 12:03:46 +0300

    Control using Transform, not Rigidbody

--- Assets/Scripts/SoldierController.cs
+++ Assets/Scripts/SoldierController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(SoldierAnimation))]
 public class SoldierController : MonoBehaviour, Controllable
 {
@@ -11,19 +10,17 @@ public class SoldierController : MonoBehaviour, Controllable
     [SerializeField]
     private float speed = 6.0f;
 
-    private Rigidbody rigidbody;
     private SoldierAnimation animator;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<SoldierAnimation>();
     }
 
     public void Move(Vector3 direction)
     {
         direction.y = 0.0f;
-        rigidbody.MovePosition(transform.position + direction.normalized * speed * Time.fixedDeltaTime);
+        transform.Translate(direction.normalized * speed * Time.fixedDeltaTime, Space.World);
         animator.AnimateMoving(direction);
     }
 
@@ -31,7 +28,7 @@ public class SoldierController : MonoBehaviour, Controllable
     {
         Vector3 direction = point - transform.position;
         direction.y = 0.0f;
-        rigidbody.MoveRotation(Quaternion.LookRotation(direction));
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 
     public void Shoot()
