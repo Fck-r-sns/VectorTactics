aaf955fa620445d3850b489bfc7a373e114e9991
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-02 00:00:56 +0300

    Cover triggers

---
+++ Assets/Scripts/Cover/BulletThroughCoverTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletThroughCoverTrigger : MonoBehaviour
+{
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(Time.time + ": Trigger, " + other.tag);
+        if (other.tag == "Bullet")
+        {
+            Bullet b = other.gameObject.GetComponent<Bullet>();
+            b.speed = 0.0f;
+        }
+    }
+}
