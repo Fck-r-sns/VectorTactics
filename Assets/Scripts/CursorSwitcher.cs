21276e5624050f59f5047ba405dcb82922364f57
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-02 01:41:06 +0300

    Crosshair cursor

---
+++ Assets/Scripts/CursorSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSwitcher : MonoBehaviour
+{
+
+    [SerializeField]
+    private Texture2D crosshair;
+
+	void Start () {
+
+        if (crosshair != null)
+        {
+            Cursor.SetCursor(
+                crosshair,
+                new Vector2(crosshair.width / 2.0f, crosshair.height / 2.0f),
+                CursorMode.Auto
+                );
+        }
+	}
+}
