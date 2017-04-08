0a61863eeb816fc58efa402041c28b6ffff96902
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-08 16:13:20 +0300

    Basic navigation

---
+++ Assets/Scripts/AI/Common/Navigation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+{
+
+    private NavMeshAgent navMeshAgent;
+    private int floorMask;
+
+    // Use this for initialization
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, floorMask))
+            {
+                Vector3 destination = hit.point;
+                destination.y = transform.position.y;
+                navMeshAgent.SetDestination(destination);
+            }
+        }
+
+    }
+}
