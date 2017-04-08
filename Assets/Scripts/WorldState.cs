114fd8856cf77ce847f86c74fcc203892cc43734
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-08 22:02:23 +0300

    World state

---
+++ Assets/Scripts/WorldState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldState : MonoBehaviour
+{
+
+    [SerializeField]
+    private SoldierController blueSoldierController;
+
+    [SerializeField]
+    private SoldierController redSoldierController;
+
+    public SoldierController blueSoldier {
+        get {
+            return blueSoldierController;
+        }
+    }
+
+    public SoldierController redSoldier {
+        get {
+            return redSoldierController;
+        }
+    }
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
