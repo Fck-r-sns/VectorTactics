2a6cf188a51a0d77b06d971772c2a2a11948f66c
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-08 19:16:01 +0300

    Namespace

--- Assets/Scripts/AI/Common/Navigation.cs
+++ Assets/Scripts/AI/Common/Navigation.cs
@@ -3,75 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(Controllable))]
-[RequireComponent(typeof(NavMeshAgent))]
-public class Navigation : MonoBehaviour
+namespace Ai
 {
 
-    private Controllable controllable;
-    private NavMeshAgent navMeshAgent;
-    private int floorMask;
-    private NavMeshPath path;
-    private Vector3? destination = null;
-
-    // Use this for initialization
-    void Start()
+    [RequireComponent(typeof(Controllable))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Navigation : MonoBehaviour
     {
-        controllable = GetComponent<Controllable>();
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        floorMask = LayerMask.GetMask("Floor");
-        path = new NavMeshPath();
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (Input.GetMouseButton(0))
+        private Controllable controllable;
+        private NavMeshAgent navMeshAgent;
+        private int floorMask;
+        private NavMeshPath path;
+        private Vector3? destination = null;
+
+        // Use this for initialization
+        void Start()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, floorMask))
+            controllable = GetComponent<Controllable>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            floorMask = LayerMask.GetMask("Floor");
+            path = new NavMeshPath();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (Input.GetMouseButton(0))
             {
-                destination = hit.point;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, floorMask))
+                {
+                    destination = hit.point;
+                }
             }
-        }
 
-        if (destination.HasValue)
-        {
-            navMeshAgent.CalculatePath(destination.Value, path);
-        }
-
-        if (path.status != NavMeshPathStatus.PathInvalid && path.corners.Length > 0)
-        {
-            Vector3? nextPoint = GetNextPoint();
-            if (nextPoint.HasValue)
+            if (destination.HasValue)
             {
-                controllable.Move(nextPoint.Value - transform.position);
+                navMeshAgent.CalculatePath(destination.Value, path);
+            }
+
+            if (path.status != NavMeshPathStatus.PathInvalid && path.corners.Length > 0)
+            {
+                Vector3? nextPoint = GetNextPoint();
+                if (nextPoint.HasValue)
+                {
+                    controllable.Move(nextPoint.Value - transform.position);
+                }
+                else
+                {
+                    controllable.Move(Vector3.zero);
+                }
             }
             else
             {
                 controllable.Move(Vector3.zero);
             }
         }
-        else
+
+        private Vector3? GetNextPoint()
         {
-            controllable.Move(Vector3.zero);
+            for (int i = 0; (i < path.corners.Length); ++i)
+            {
+                Vector3 nextPoint = path.corners[i];
+                nextPoint.y = 0;
+                Vector3 currentPosition = transform.position;
+                currentPosition.y = 0;
+                if (Vector3.Distance(nextPoint, currentPosition) > 0.1f)
+                {
+                    return nextPoint;
+                }
+            };
+            return null;
         }
     }
 
-    private Vector3? GetNextPoint()
-    {
-        for (int i = 0; (i < path.corners.Length); ++i)
-        {
-            Vector3 nextPoint = path.corners[i];
-            nextPoint.y = 0;
-            Vector3 currentPosition = transform.position;
-            currentPosition.y = 0;
-            if (Vector3.Distance(nextPoint, currentPosition) > 0.1f)
-            {
-                return nextPoint;
-            }
-        };
-        return null;
-    }
 }
