34a2bd0bc33c09d19d88cbf359dd39d3061a2912
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-09 15:55:22 +0300

    Fps GUI

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -12,6 +12,8 @@ public class GUI : MonoBehaviour, IEventSubscriber
     private Text blueSoldierHealthText;
     private Slider redSoldierHealthSlider;
     private Text redSoldierHealthText;
+    private Text logicFps;
+    private Text physicsFps;
 
     private int address = AddressProvider.GetFreeAddress();
 
@@ -33,6 +35,8 @@ public class GUI : MonoBehaviour, IEventSubscriber
         blueSoldierHealthText = transform.Find("Canvas/BlueSoldier/BlueSoldierHealthText").gameObject.GetComponent<Text>();
         redSoldierHealthSlider = transform.Find("Canvas/RedSoldier/RedSoldierHealthSlider").gameObject.GetComponent<Slider>();
         redSoldierHealthText = transform.Find("Canvas/RedSoldier/RedSoldierHealthText").gameObject.GetComponent<Text>();
+        logicFps = transform.Find("Canvas/Fps/LogicFps").gameObject.GetComponent<Text>();
+        physicsFps = transform.Find("Canvas/Fps/PhysicsFps").gameObject.GetComponent<Text>();
 
         Dispatcher.Subscribe(EBEventType.BlueSoldierHealthChanged, address, gameObject);
         Dispatcher.Subscribe(EBEventType.RedSoldierHealthChanged, address, gameObject);
@@ -44,9 +48,11 @@ public class GUI : MonoBehaviour, IEventSubscriber
         Dispatcher.Unsubscribe(EBEventType.RedSoldierHealthChanged, address);
     }
 
-    void Update () {
-		
-	}
+    void Update ()
+    {
+        logicFps.text = "" + 1.0f / Time.deltaTime;
+        physicsFps.text = "" + 1.0f / Time.fixedDeltaTime;
+    }
 
     private void SetBlueSoldierHealth(float value)
     {
