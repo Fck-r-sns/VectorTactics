7ea0c840bc2f58d1e81ee6b816cdc91b969c7f3c
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-09 21:38:52 +0300

    Defines

--- Assets/Scripts/Defines.cs
+++ Assets/Scripts/Defines.cs
@@ -5,4 +5,11 @@
         Blue,
         Red
     }
+
+    public const float MAP_WIDTH = 30.0f;
+    public const float MAP_HEIGHT = 30.0f;
+
+    public const float FULL_HP = 100.0f;
+    public const float MEDIUM_HP = 50.0f;
+    public const float LOW_HP = 25.0f;
 }
