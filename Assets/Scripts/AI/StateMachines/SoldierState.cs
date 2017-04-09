602617d0bd60a832e9e95fa626ed6a247568442c
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-09 14:46:04 +0300

    Soldier base state

---
+++ Assets/Scripts/AI/StateMachines/SoldierState.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Ai
+{
+
+    namespace Fsm
+    {
+
+        public abstract class SoldierState : State
+        {
+            private WorldState world;
+            private TerrainReasoning terrain;
+            private SoldierController controller;
+
+            public SoldierState(WorldState world, TerrainReasoning terrain, SoldierController controller)
+            {
+                this.world = world;
+                this.terrain = terrain;
+                this.controller = controller;
+            }
+        }
+
+    }
+}
