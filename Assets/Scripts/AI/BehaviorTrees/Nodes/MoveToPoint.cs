18ad0e22482a44aa04155932cfe2ffc04a52c8e7
Fck-r-sns <fck.r.sns@gmail.com>
2017-05-07 18:57:16 +0300

    New nodes

---
+++ Assets/Scripts/AI/BehaviorTrees/Nodes/MoveToPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Ai
+{
+    namespace Bt
+    {
+        public class MoveToPoint : SoldierNode
+        {
+            private string pointVariable;
+
+            public MoveToPoint(Environment environment, AiTools aiTools, string pointVariable) : base(environment, aiTools)
+            {
+                this.pointVariable = pointVariable;
+            }
+
+            public override Result Run()
+            {
+                if (!environment.ContainsValue(pointVariable))
+                {
+                    return Result.Failure;
+                }
+                Vector3 point = environment.GetValue<Vector3>(pointVariable);
+                aiTools.navigation.SetDestination(point);
+                return Result.Success;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/BehaviorTrees/Nodes/SelectDestinationPoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ai
+{
+    namespace Bt
+    {
+        public class SelectDestinationPoint : SoldierNode
+        {
+            private string searchRadiusVariable;
+            private string minWeightVariable;
+            private string resultVariable;
+
+            public SelectDestinationPoint(Environment environment, AiTools aiTools, string searchRadiusVariable, string minWeightVariable, string resultVariable) : base(environment, aiTools)
+            {
+                this.searchRadiusVariable = searchRadiusVariable;
+                this.minWeightVariable = minWeightVariable;
+                this.resultVariable = resultVariable;
+            }
+
+            public override Result Run()
+            {
+                if (!environment.ContainsValue(searchRadiusVariable) || !environment.ContainsValue(minWeightVariable))
+                {
+                    return Result.Failure;
+                }
+                float searchRadius = environment.GetValue<float>(searchRadiusVariable);
+                float minWeight = environment.GetValue<float>(minWeightVariable);
+                Vector3 result;
+                List<Waypoint> wps = aiTools.terrain.GetGoodWaypoints(searchRadius, minWeight);
+                if (wps.Count == 0)
+                {
+                    result = new Vector3(
+                        aiTools.agentState.position.x + Random.Range(-searchRadius, searchRadius),
+                        0.0f,
+                        aiTools.agentState.position.z + Random.Range(-searchRadius, searchRadius)
+                    );
+                }
+                else
+                {
+                    int index = Random.Range(0, wps.Count - 1);
+                    result = wps[index].position;
+                }
+                environment.SetValue(resultVariable, result);
+                return Result.Success;
+            }
+        }
+    }
+}
