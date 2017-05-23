6b3e8f0bc348f58d897a91f6762ece86fb028ea8
Fck-r-sns <fck.r.sns@gmail.com>
2017-05-23 22:10:19 +0300

    Game restarter

---
+++ Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using EventBus;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Statistics))]
+public class GameFlowManager : MonoBehaviour, IEventSubscriber
+{
+    private Statistics statistics;
+
+    public void OnReceived(EBEvent e)
+    {
+        if (e.type == EBEventType.HealthChanged)
+        {
+            if ((e as HealthChanged).value <= 0.0f)
+            {
+                StartCoroutine(FinishAndRestart());
+            }
+        }
+    }
+
+    void Awake()
+    {
+        statistics = GetComponent<Statistics>();
+    }
+
+    IEnumerator FinishAndRestart()
+    {
+        statistics.WriteToFile();
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+}
